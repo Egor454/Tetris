0e44cea80f0222217c843d9bbc5a2fc06b9673f4
Egor454 <39384311+Egor454@users.noreply.github.com>
2020-07-03 17:39:35 +0300

    Исправил отображение 2 игрока при разном разрешении

---
+++ Tetris_NEw/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviourSingleton<CameraControl>
+{
+    [SerializeField] private Camera camera;
+    public void ChangeSizeCamera(int value)
+    {
+        camera.orthographicSize = value;
+    }
+
+  
+}
--- Tetris_NEw/Assets/Scripts/LevelGenerate.cs
+++ Tetris_NEw/Assets/Scripts/LevelGenerate.cs
@@ -4,7 +4,6 @@ public class LevelGenerate : MonoBehaviour
 {
     private GameObject singleplayer;
     private GameObject twoplayers;
-
     [SerializeField] private GameObject prefabGridFirstPlayer;
     [SerializeField] private GameObject prefabGridSecondPlayer;
     void Start()
@@ -16,11 +15,15 @@ public class LevelGenerate : MonoBehaviour
         if (GlobalScore.Instance.NumberPlayers == 0)
         {
             singleplayer = Instantiate(prefabGridFirstPlayer, new Vector2(20.0f, 0.0f), Quaternion.identity);
+            CameraControl.Instance.ChangeSizeCamera(15);
+
+
         }
         else
         {
             singleplayer = Instantiate(prefabGridFirstPlayer, new Vector2(20.0f, 0.0f), Quaternion.identity);
             twoplayers = Instantiate(prefabGridSecondPlayer, new Vector2(0.0f, 0.0f), Quaternion.identity);
+            CameraControl.Instance.ChangeSizeCamera(22);
         }
     }
 }
